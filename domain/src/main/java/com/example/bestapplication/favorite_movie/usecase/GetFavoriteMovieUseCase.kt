fa4d187bc61c9212113385c4b8f8b28d1232dc7b
Sergey Rudman <sergey02047747@gmail.com>
2022-04-08 12:50:35 +0300

    fix di

--- domain/src/main/java/com/example/bestapplication/favorite_movie/usecase/GetFavoriteMovieUseCase.kt
+++ domain/src/main/java/com/example/bestapplication/favorite_movie/usecase/GetFavoriteMovieUseCase.kt
@@ -1,9 +1,8 @@
 package com.example.bestapplication.favorite_movie.usecase
 
 import com.example.bestapplication.repository.FavoriteMovieRepository
-import javax.inject.Inject
 
-class GetFavoriteMovieUseCase @Inject constructor(private val movieRepository: FavoriteMovieRepository) {
+class GetFavoriteMovieUseCase (private val movieRepository: FavoriteMovieRepository) {
 
     suspend fun invoke() = movieRepository.getFavoriteMovie()
 
